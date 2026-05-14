dfd44d1be16d9abce0a3869499298b590e6f9d63
Uzair <uzairpmd@gmail.com>
2026-05-14 18:13:35 +0530

    Delete largest.py

--- largest.py
+++
@@ -1,5 +0,0 @@
-#Write a program to find the largest of three numbers.
-def  largest(a,b,c):
-    largest_num= a if a>b and a>c else b if b>a and b>c else c
-    return largest_num
-print(largest(100,10,4))
