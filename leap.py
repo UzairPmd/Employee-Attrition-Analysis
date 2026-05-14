0886f7a05d6df5a052b8093636f1f396621c4cf4
Uzair <uzairpmd@gmail.com>
2026-05-14 18:13:47 +0530

    Delete leap.py

--- leap.py
+++
@@ -1,7 +0,0 @@
-#Write a program to check whether a given year is a leap year.
-def leap(year):
-    return True if (year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)) else False
-print(leap(2000))  
-print(leap(1900))  
-print(leap(2024))  
-print(leap(2023))  
