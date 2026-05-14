2e6ae4602c5eb208e80447212cd6564e21d58c03
Uzair <uzairpmd@gmail.com>
2026-05-14 18:14:34 +0530

    Delete sum.py

--- sum.py
+++
@@ -1,9 +0,0 @@
-#Write a program to calculate the sum of first N natural numbers using a while loop.
-def sum_of_natural(n):
-    total = 0
-    while n > 0:
-        total += n
-        n -= 1 
-    return total
-print(sum_of_natural(5))   
-print(sum_of_natural(10))  
