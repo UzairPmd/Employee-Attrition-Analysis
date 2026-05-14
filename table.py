b3cee1a26434789263e7fa86646a86f2ecea9bc9
Uzair <uzairpmd@gmail.com>
2026-05-14 18:14:21 +0530

    Delete table.py

--- table.py
+++
@@ -1,8 +0,0 @@
-#Write a program to print the multiplication table of a given number using a while loop.
-def multi_table(n):
-    i=1
-    while i<11:
-        print(f"{n}*{i}={n*i}")
-        i+=1
-multi_table(5)
-multi_table(21)
